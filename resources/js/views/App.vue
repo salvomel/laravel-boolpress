4a9eaf229e2219c4f6ebe3f3718b18d68c49fdba
salvomel <salv.mel@gmail.com>
2022-02-21 15:54:35 +0100

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <WorkInProgress />
+    </div>
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue';
+
+export default {
+    name: 'App',
+    components: {
+        WorkInProgress
+    }
+}
+</script>
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <section>
+        <div class="container">
+            <h1>Area protetta</h1>
+        </div>
+    </section>
+@endsection
