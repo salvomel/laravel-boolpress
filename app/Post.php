d54c6731bb652bb6eb6fe9d725186b74c404528d
salvomel <salv.mel@gmail.com>
2022-02-25 15:47:02 +0100

    Tags migration and seeder

--- app/Post.php
+++ app/Post.php
@@ -18,6 +18,10 @@ class Post extends Model
         return $this->belongsTo('App\Category');
     }
 
+    public function tags() {
+        return $this->belongsToMany('App\Tag');
+    }
+
     // Funzione per Slug univoco
     public static function getUniqueSlugFromTitle($title) {
         
