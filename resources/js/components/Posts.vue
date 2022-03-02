1cd039dbb73ef6a6bb82ce96a49fff730e551b49
salvomel <salv.mel@gmail.com>
2022-03-02 15:35:30 +0100

    Posts api and show content in front office

---
+++ resources/js/components/Posts.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <h1>Posts</h1>
+
+        <div class="row row-cols-3">
+            <div v-for="post in posts" :key="post.id" class="col">
+                    <div class="card my-2">
+                        
+                        <div class="card-body">
+                            <h4 class="card-title">{{ post.title }}</h4>
+                            <p class="card-text">{{ post.content }}</p>
+                        </div>
+                        
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Posts',
+    data: function() {
+        return {
+            posts: []
+        };
+    },
+    methods: {
+        getPosts: function() {
+            axios.get('api/posts')
+            .then((response) => {
+                this.posts = response.data.results;
+            });
+        },
+    },
+    created: function() {
+        this.getPosts();
+    }
+}
+</script>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
-        <WorkInProgress />
+        <Posts />
     </div>
 </template>
 
 <script>
-import WorkInProgress from '../components/WorkInProgress.vue';
+import Posts from '../components/Posts.vue';
 
 export default {
     name: 'App',
     components: {
-        WorkInProgress
+        Posts
     }
 }
 </script>
